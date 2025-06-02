63e981d236b30bb7fda7a22aa87049428beb6cda
Guilherme Souza <guilhermesouza8421@gmail.com>
2025-06-02 03:01:30 +0000

    consegui mudar o plano, mas ao publicar esta dando esse erro: Something went wrong creating your App Hosting rollout. Please try re-publishing.

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'standalone', // Add this line
 };
 
 export default nextConfig;
